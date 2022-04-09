692d073ad5838b0ffd823fb6fd7fbac72406c652
JKess731 <62125526+JKess731@users.noreply.github.com>
2022-04-09 18:09:12 -0400

    completes project

---
+++ Global.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var menu = null
+
+func _ready():
+	pause_mode = Node.PAUSE_MODE_PROCESS
+
+func _unhandled_input(_event):
+	if Input.is_action_just_pressed("menu"):
+		if menu == null:
+			menu = get_node_or_null("/root/Game/UI/Menu")
+		if menu != null:
+			if not menu.visible:
+				menu.show()
+				get_tree().paused = true
+			else:
+				menu.hide()
+				get_tree().paused = false
